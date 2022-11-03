52522439d6e8a5edb93ef60940f3752dbbc44d7b
Brian Hill <51302710+TrojanPinata@users.noreply.github.com>
2022-11-03 14:24:50 -0400

    initial commit

---
+++ followlist.py
@@ -0,0 +1,31 @@
+import tweepy
+import logging
+import os
+
+# debug code: please do not remove - just comment out
+logger = logging.getLogger("tweepy")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="tweepy.log")
+logger.addHandler(handler)
+
+# plug your own details in here
+consumer_key = 1
+consumer_secret = 1
+access_token = 1
+access_token_secret = 1
+
+auth = tweepy.OAuth1UserHandler(
+   consumer_key, consumer_secret, access_token, access_token_secret
+)
+
+api = tweepy.API(auth)
+
+# code start
+# open file
+
+ids = [0]
+
+# break urls into user id's
+for i in range(len(ids)):
+   id = ids(i) 
+   api.create_friendship(user_id = id, follow = false)
